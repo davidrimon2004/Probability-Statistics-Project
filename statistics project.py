edaed74723ff565a1cfafe246de15c62a7a42092
David Rimon Halim <96887854+davidrimon2004@users.noreply.github.com>
2024-12-02 23:24:58 +0200

    Update statistics project.py
    added function Bar for the bar graphs and stored 3 of the 9 columns in the project code to access it later 
    
    (The read will probably be made into function to make it easier)

--- statistics project.py
+++ statistics project.py
@@ -2,4 +2,13 @@ import math
 import statistics as stat
 import pandas as pd
 data ="Diabetes Dataset.csv"
-
+def Bar(x,y,IndX,IndY):
+    mat.bar(x,y)
+    mat.xlabel(read.columns[IndX])
+    mat.ylabel(read.columns[IndY])
+    mat.show()
+data ="Diabetes Dataset.csv"
+read=pd.read_csv(data)
+preg=read['Pregnancies']         #index=0
+gloc=read['Glucose']             #index=1
+pressure=read['BloodPressure']   #index=2
