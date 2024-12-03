47bb1fc62088cee8f8a8e8c98cfe4a2b7fe0b7ae
David Rimon Halim <96887854+davidrimon2004@users.noreply.github.com>
2024-12-03 22:50:55 +0200

    Update statistics project.py
    pie chart

--- statistics project.py
+++ statistics project.py
@@ -18,6 +18,12 @@ bmi=read[read.columns[5]]
 diabetes=read[read.columns[6]]
 age=read[read.columns[7]]
 outcome=read[read.columns[8]]
+#plot correalation matrix
 plot=read.corr()
 sb.heatmap(plot, annot=True, cmap='', fmt=".2f", cbar=True)
+visual.show()
+#piechart
+ct=read[read.columns[8]].value_counts()
+visual.pie(ct,labels=['0','1'])
+visual.legend()
 visual.show()
